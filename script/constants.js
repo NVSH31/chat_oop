d7de9fe094f7c0e4af2e943cea3651c7ea930747
Natasha Shpak <n.v.shpak@yandex.ru>
2023-01-05 13:46:51 +0400

    fix start page

--- script/constants.js
+++ script/constants.js
@@ -2,11 +2,11 @@ export const startMessages = [
   {
     user: 'i',
     text: 'Привет, это первое сообщение текущего пользователя, т.е. Вас.',
-    image: '../images/1.png',
+    image: './images/1.png',
   },
   {
     user: 'friend',
     text: 'Первое сообщение собеседника.',
-    image: '../images/2.png',
+    image: './images/2.png',
   }
 ];
